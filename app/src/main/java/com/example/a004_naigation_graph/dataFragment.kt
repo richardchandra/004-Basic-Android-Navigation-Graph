f750a80687f4ff5f644272dbc06ae3e7c0cb0c06
unknown <richardchandra99@gmail.com>
2022-03-16 00:55:11 +0700

    Initial commit for navigation graph learning hands-on code

---
+++ app/src/main/java/com/example/a004_naigation_graph/dataFragment.kt
@@ -0,0 +1,30 @@
+package com.example.a004_naigation_graph
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class dataFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_data, container, false)
+        val button = view.findViewById<Button>(R.id.button2)
+        button.setOnClickListener{
+            findNavController().navigate(R.id.action_dataFragment_to_homeFragment)
+        }
+
+        return view
+    }
+
+}
